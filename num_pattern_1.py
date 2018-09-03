ca1075dfe2a3a40aeb41dcce6d2ac8cb22d135f2
Arvind Sharma <avin001@gmail.com>
2018-09-03 16:10:05 +0530

    Updated the file num_pattern_1.py
    Updated the file num_pattern_1.py

--- num_pattern_1.py
+++ num_pattern_1.py
@@ -23,15 +23,16 @@ Sample Output 1 :
 """
 
 n = int(input())
+
 for a in range(1, n + 1):
-  for b in range(1, a + 1):
-    if b <= a:
-      print(b, end="")
-    else:
-      print(" ", end="")
-  for c in range(n, 0, -1):
-    if c > a:
-      print(" ", end="")
-    else:
-      print(c, end="")
-  print()
+    for b in range(1, a + 1):
+        if b <= a:
+            print(b, end="")
+        else:
+            print(" ", end="")
+    for c in range(n, 0, -1):
+        if c > a:
+            print(" ", end="")
+        else:
+            print(c, end="")
+    print()
