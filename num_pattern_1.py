c0b13322cbc66c479ce7da691fafd0fc279489e8
Arvind Sharma <avin001@gmail.com>
2018-09-03 12:05:13 +0530

    Added comments to the file num_pattern_1.py
    Added comments to the file num_pattern_1.py

--- num_pattern_1.py
+++ num_pattern_1.py
@@ -1,3 +1,27 @@
+"""
+Print the following pattern for n number of rows.
+
+For eg. N = 5
+
+1    1
+12   21
+123  321
+1234 4321
+1234554321
+
+Sample Input 1 :
+
+4
+
+Sample Output 1 :
+
+1   1
+12  21
+123 321
+12344321
+
+"""
+
 n = int(input())
 for a in range(1, n + 1):
   for b in range(1, a + 1):
